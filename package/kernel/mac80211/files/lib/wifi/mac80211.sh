d7a8b1ffaa6a978d4b6c56b6866af1361647b95a
thl <meisreallyba@gmail.com>
2022-05-26 13:25:45 +0800

    update mac80211 and mt76

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -174,14 +174,14 @@ detect_mac80211() {
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio${devidx}.country=US
 			set wireless.radio${devidx}.disabled=0
+ 			set wireless.radio${devidx}.country=US
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.ssid=OpenWrt_5G
 			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
